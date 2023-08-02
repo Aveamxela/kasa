dd98782bbd4fc1f35613374b0b4ff2596b5d5f43
Maeva <maeva-froger@outlook.fr>
2023-08-02 14:15:03 +0200

    locations_home

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import data from "../Data.json";
 
 const Locations = () => {
     return (
-        <div className='locations'>
-            <h1>Locations</h1>
+        <div className="locations">
+            {data.map((data, i) => (
+                <div key={i} className="location-item">
+                    <div
+                        className="img-location-home"
+                        style={{ backgroundImage: `url('${data.cover}')` }}
+                    ></div>
+                    <div className="linear-gradient">
+                        <h1 className="title-location">{data.title}</h1>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
