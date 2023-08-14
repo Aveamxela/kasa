1902d69691a7a1f5b8ffeae8bb2607400a2472d3
Maeva <maeva-froger@outlook.fr>
2023-08-14 13:27:08 +0200

    setup gh-pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import Error from "../pages/Error";
 
 function App() {
     return (
-        <BrowserRouter basename={'/kasa'}>
+        <BrowserRouter basename={'/kasa/'}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
